f1a6924735905bfc8f4bfb2f0557a044e3dac75c
Daydeus <JL02Murphy@gmail.com>
2022-10-20 14:47:08 -0500

    Starter Pokemon Held-item to Exp Share

--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -216,7 +216,7 @@ CyndaquilPokeBallScript:
 	writetext ChoseStarterText
 	promptbutton
 	waitsfx
-	givepoke CYNDAQUIL, NO_FORM, 5, ORAN_BERRY
+	givepoke CYNDAQUIL, NO_FORM, 5, EXP_SHARE
 	writetext LyraChoosesStarterText
 	waitbutton
 	closetext
@@ -256,7 +256,7 @@ TotodilePokeBallScript:
 	writetext ChoseStarterText
 	promptbutton
 	waitsfx
-	givepoke TOTODILE, NO_FORM, 5, ORAN_BERRY
+	givepoke TOTODILE, NO_FORM, 5, EXP_SHARE
 	writetext LyraChoosesStarterText
 	waitbutton
 	closetext
@@ -294,7 +294,7 @@ ChikoritaPokeBallScript:
 	writetext ChoseStarterText
 	promptbutton
 	waitsfx
-	givepoke CHIKORITA, NO_FORM, 5, ORAN_BERRY
+	givepoke CHIKORITA, NO_FORM, 5, EXP_SHARE
 	writetext LyraChoosesStarterText
 	waitbutton
 	closetext
